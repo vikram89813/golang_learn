19abeb403cfc9692a651ba4447ccc206ab53bb9e
Kumar Vikram <kumar@circles.asia>
2019-09-08 19:14:05 +0800

    second_commit

---
+++ 001-hello-world/test_mod.go
@@ -0,0 +1,8 @@
+package  main
+//package hello
+//
+//import "rsc.io/quote"
+//
+//func Hello() string {
+//	return quote.Hello()
+//}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example/test/repo
+
+go 1.12
+
+require rsc.io/quote v1.5.2
